2400ed9eabd056d9c2e39a858697258c44e87ef8
Деменко Вячеслав Павлович ИП-513 <demenko_slava@mail.ru>
2025-10-26 20:29:54 +0700

    Реализован класс Phone

---
+++ src/com/sibsutis/devices/Phone.java
@@ -0,0 +1,42 @@
+package com.sibsutis.devices;
+
+import java.util.Objects;
+
+public class Phone extends Device {
+    
+    public Phone(int id, int price) {
+        super(id, price);
+    }
+    
+    public Phone(int id, int price, String ip) {
+        super(id, price, ip);
+    }
+    
+    @Override
+    public String getDeviceType() {
+        return "Phone";
+    }
+    
+    @Override
+    public String sprint() {
+        return "Phone{" +
+                "id=" + id +
+                ", price=" + price +
+                ", ip='" + (ip != null ? ip : "null") + '\'' +
+                '}';
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Phone phone = (Phone) o;
+        return Objects.equals(ip, phone.ip);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), ip);
+    }
+}
